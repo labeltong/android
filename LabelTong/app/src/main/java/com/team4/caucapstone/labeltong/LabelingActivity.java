cae3585b57e062c5f593e0a06b1c31ad35ef0fdb
Kim SangHeon <comnamu18@gmail.com>
2019-03-24 15:46:52 +0900

    Basic Activity Created
    Main, labeling, settings pages are created

---
+++ LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/LabelingActivity.java
@@ -0,0 +1,13 @@
+package com.team4.caucapstone.labeltong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LabelingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labeling);
+    }
+}
