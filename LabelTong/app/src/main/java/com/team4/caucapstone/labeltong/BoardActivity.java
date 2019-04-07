998e4cdbca5e12fee167ff276163aaa06cee42ee
Kim SangHeon <comnamu18@gmail.com>
2019-04-07 21:49:30 +0900

    Label Img Function
    Img Label View complete

--- LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/BoardActivity.java
+++ LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/BoardActivity.java
@@ -9,14 +9,37 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class BoardActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class BoardActivity extends AppCompatActivity{
     FragmentManager manager = getSupportFragmentManager();
     final Fragment boardFragment = new BoardFragment();
     //final Fragment settingFragment = new SettingFragment();
     FragmentTransaction transaction;
     Fragment active = boardFragment;
+    private ImageButton method_bounding;
+    private ImageButton method_classify;
+    private ImageButton method_sentiment;
+
+    //ToDo : Buttons must be determined by server
+    private ImageButton topic_car;
+    private ImageButton topic_cat;
+    private ImageButton topic_roadsign;
+    private ImageButton topic_emotion;
+
+    public final static int INTENT_ERROR = -1;
+    public final static int METHOD_BOUNDING  = 1;
+    public final static int METHOD_CLASSIFY  = 2;
+    public final static int METHOD_SENTIMENT = 3;
+    //ToDo : When Server ready, change it to dynamic values
+    public final static int TOPIC_CAT = 4;
+    public final static int TOPIC_CAR = 5;
+    public final static int TOPIC_ROADSIGN = 6;
+    public final static int TOPIC_EMOTION = 7;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -53,6 +76,46 @@ public class BoardActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        method_bounding  = (ImageButton) findViewById(R.id.boundingBox);
+        method_classify  = (ImageButton) findViewById(R.id.classification);
+        method_sentiment = (ImageButton) findViewById(R.id.sentiment);
+
+        //ToDo: Get Topic Information from server
+        topic_roadsign = (ImageButton)findViewById(R.id.roadsign);
+        topic_cat = (ImageButton)findViewById(R.id.cat);
+        topic_car = (ImageButton)findViewById(R.id.car);
+        topic_emotion = (ImageButton)findViewById(R.id.emotion);
+
+    }
+
+    public void buttonClicked(View v) {
+        Intent intent = new Intent(BoardActivity.this, LabelingActivity.class);
+        switch (v.getId()) {
+            case R.id.boundingBox:
+                intent.putExtra("INFO", METHOD_BOUNDING);
+                break;
+            case R.id.classification:
+                intent.putExtra("INFO", METHOD_CLASSIFY);
+                break;
+            case R.id.sentiment:
+                intent.putExtra("INFO", METHOD_SENTIMENT);
+                break;
+            case R.id.cat:
+                intent.putExtra("INFO", TOPIC_CAT);
+                break;
+            case R.id.car:
+                intent.putExtra("INFO", TOPIC_CAR);
+                break;
+            case R.id.roadsign:
+                intent.putExtra("INFO", TOPIC_ROADSIGN);
+                break;
+            case R.id.emotion:
+                intent.putExtra("INFO", TOPIC_EMOTION);
+                break;
+        }
+        startActivity(intent);
     }
 
 }
--- LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/LabelingActivity.java
+++ LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/LabelingActivity.java
@@ -1,13 +1,95 @@
 package com.team4.caucapstone.labeltong;
 
+import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static com.team4.caucapstone.labeltong.BoardActivity.*;
 
 public class LabelingActivity extends AppCompatActivity {
+    //FragmentManager manager = getSupportFragmentManager();
+    //final Fragment imgLabeling = new ImgFragment();
+    //FragmentTransaction transaction;
+    ImageView imgView;
+    TextView headerText;
+    TextView questionText;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_labeling);
+
+        headerText = (TextView) findViewById(R.id.labelImgTitle);
+        questionText = (TextView) findViewById(R.id.labelImgQuestion);
+        imgView = (ImageView) findViewById(R.id.labelImg);
+
+        imgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] values = new int[2];
+                int[] test = new int[2];
+                v.getLocationInWindow(values);
+                v.getLocationOnScreen(test);
+
+                Log.d("X&Y in WINDOW", values[0] + " " + values[1]);
+                Log.d("X&Y in SCREEN", test[0] + " " + test[1]);
+            }
+        });
+
+        radioGroup = (RadioGroup) findViewById(R.id.labelImgRadio);
+        radioGroup.setOrientation(RadioGroup.HORIZONTAL);
+        setLabelSettings();
+
+        //transaction = manager.beginTransaction();
+        //transaction.add(R.id.frameLayout, settingFragment).hide(settingFragment);
+        //transaction.add(R.id.labelFrame, imgLabeling).commit();
+
     }
+
+    private void setLabelSettings(){
+        switch (getIntent().getIntExtra("INFO", INTENT_ERROR)){
+            case METHOD_BOUNDING:
+                headerText.setText("Bounding BOX");
+                questionText.setText("PLEASE BOUND AREA ABOUT TEST");
+                for (int i = 0; i < 4; i++) {
+                    RadioButton radioButton = new RadioButton(this);
+                    radioButton.setText("Point" + (i+1));
+                    radioGroup.addView(radioButton);
+                }
+                break;
+            case METHOD_CLASSIFY:
+                headerText.setText("Clssification");
+                break;
+            case METHOD_SENTIMENT:
+                headerText.setText("Sentiment");
+                break;
+            case TOPIC_CAT:
+                headerText.setText("Cat");
+                break;
+            case TOPIC_CAR:
+                headerText.setText("Car");
+                break;
+            case TOPIC_ROADSIGN:
+                headerText.setText("Road Sign");
+                break;
+            case TOPIC_EMOTION:
+                headerText.setText("Emotion");
+                break;
+        }
+    }
+
 }
