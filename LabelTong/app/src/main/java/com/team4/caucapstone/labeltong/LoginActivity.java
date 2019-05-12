fcbe7e1b2ccf9d41524ab00e15f5162a5df2f5f6
Kim SangHeon <comnamu18@gmail.com>
2019-05-12 11:48:58 +0900

    Login&Signup redesign
    Design Update in Login & Signup UI

--- LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/LoginActivity.java
+++ LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/LoginActivity.java
@@ -5,15 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int REQUEST_SIGNUP = 0;
+
+    EditText emailText;
+    Button loginButton;
+    TextView signupLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = (Button)findViewById(R.id.loginButton);
+
+        Button loginButton = (Button)findViewById(R.id.btn_login);
         loginButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -22,12 +30,25 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
-        Button createButton = (Button)findViewById(R.id.createButton);
-        createButton.setOnClickListener(new Button.OnClickListener(){
+
+        signupLink = (TextView) findViewById(R.id.link_signup);
+        signupLink.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view){
-                // ToDo : Social Login Buttons
+               Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+                Intent intent = new Intent(getApplicationContext(), BoardActivity.class);
+                startActivity(intent);
+                this.finish();
+            }
+        }
     }
 }
---
+++ LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/SignupActivity.java
@@ -0,0 +1,121 @@
+package com.team4.caucapstone.labeltong;
+
+import android.app.ProgressDialog;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+    EditText nameText;
+    EditText emailText;
+    EditText phoneText;
+    Button signupButton;
+    TextView loginLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        nameText = (EditText) findViewById(R.id.input_name);
+        emailText = (EditText) findViewById(R.id.input_email);
+        phoneText = (EditText) findViewById(R.id.input_phone);
+
+        signupButton = (Button) findViewById(R.id.btn_signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        loginLink = (TextView) findViewById(R.id.link_login);
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void signup() {
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        signupButton.setEnabled(false);
+
+        final AppCompatDialog progressDialog= new AppCompatDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        progressDialog.setContentView(R.layout.progress);
+        progressDialog.show();
+
+        String name = nameText.getText().toString();
+        String email = emailText.getText().toString();
+        String phone = phoneText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Sign up failed", Toast.LENGTH_LONG).show();
+        signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = nameText.getText().toString();
+        String email = emailText.getText().toString();
+        String phone = phoneText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            nameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            nameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            emailText.setError(null);
+        }
+
+        if (phone.isEmpty() || phone.length() != 11) {
+            phoneText.setError("enter valid phone number without -");
+            valid = false;
+        } else {
+            phoneText.setError(null);
+        }
+
+        return valid;
+    }
+}
