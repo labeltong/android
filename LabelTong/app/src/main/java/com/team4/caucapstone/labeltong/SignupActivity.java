d5d6958ee67666b722c772756aa8b994e0b28b37
Kim SangHeon <comnamu18@gmail.com>
2019-05-12 13:56:30 +0900

    Facebook CreateAccount
    Create Account with Facebook OAUTH Doen

--- LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/SignupActivity.java
+++ LabelTong/app/src/main/java/com/team4/caucapstone/labeltong/SignupActivity.java
@@ -1,31 +1,90 @@
 package com.team4.caucapstone.labeltong;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class SignupActivity extends AppCompatActivity {
+    static final int FACEBOOK = 1;
+
     EditText nameText;
     EditText emailText;
     EditText phoneText;
+    Button fbAuthButton;
     Button signupButton;
     TextView loginLink;
+
+    private static final boolean isConnectionDone = false;
+    private static final ArrayList<String> PERMISSIONS = new ArrayList<>();
+    private static final String AUTH_COMPLETE = "Authorized!";
+    private static final String POST_URL = "http://54.180.195.179:19432/auth";
+
+    CallbackManager callbackManager;
+    Activity currentActivity = this;
+    String authToken;
+    String name;
+    String email;
+    String phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,  new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+               authToken = loginResult.getAccessToken().getToken();
+                fbAuthButton.setText(AUTH_COMPLETE);
+            }
+            @Override
+            public void onCancel() {
+                Toast.makeText(SignupActivity.this, "Login Cancel", Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(SignupActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
         nameText = (EditText) findViewById(R.id.input_name);
         emailText = (EditText) findViewById(R.id.input_email);
         phoneText = (EditText) findViewById(R.id.input_phone);
+        fbAuthButton = (Button) findViewById(R.id.btn_fb_auth);
+        fbAuthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(currentActivity, Arrays.asList("email"));
+            }
+        });
 
         signupButton = (Button) findViewById(R.id.btn_signup);
         signupButton.setOnClickListener(new View.OnClickListener() {
@@ -34,7 +93,6 @@ public class SignupActivity extends AppCompatActivity {
                 signup();
             }
         });
-
         loginLink = (TextView) findViewById(R.id.link_login);
         loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,22 +116,34 @@ public class SignupActivity extends AppCompatActivity {
         progressDialog.setContentView(R.layout.progress);
         progressDialog.show();
 
-        String name = nameText.getText().toString();
-        String email = emailText.getText().toString();
-        String phone = phoneText.getText().toString();
-
-        // TODO: Implement your own signup logic here.
 
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
+                        if (postToServer()) {
+                            onSignupSuccess();
+                        }
+                        else {
+                            onSignupFailed();
+                        }
                         progressDialog.dismiss();
                     }
                 }, 3000);
+    }
+    private boolean postToServer() {
+        JSONObject postData = new JSONObject();
+        try{
+            postData.put("email", email);
+            postData.put("token", authToken);
+            postData.put("name", name);
+            postData.put("phone_num", phone);
+            Log.d("TOKENTEST", postData.toString());
+            new SendDeviceDetails().execute(POST_URL, postData.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
 
@@ -91,9 +161,9 @@ public class SignupActivity extends AppCompatActivity {
     public boolean validate() {
         boolean valid = true;
 
-        String name = nameText.getText().toString();
-        String email = emailText.getText().toString();
-        String phone = phoneText.getText().toString();
+        name = nameText.getText().toString();
+        email = emailText.getText().toString();
+        phone = phoneText.getText().toString();
 
         if (name.isEmpty() || name.length() < 3) {
             nameText.setError("at least 3 characters");
@@ -116,6 +186,17 @@ public class SignupActivity extends AppCompatActivity {
             phoneText.setError(null);
         }
 
+        if (authToken.isEmpty()) {
+            Log.d("TOKKEN ERROR", "MISSING  TOKEN");
+            valid = false;
+        }
+
         return valid;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
